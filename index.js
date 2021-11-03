11f2457f09576654b2e2c75e2fddf235f44ddebc
Omar <morenoomar29@yahoo.com>
2021-11-03 11:04:06 -0500

    commit tree 64bee0be33bf1bbae0d647f2900abbf25a171a0a

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+function saturdayFun(act = "roller-skate") {
+    return `This Saturday, I want to ${act}!`
+}
+//saturdayFun()
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+
+function wrapAdjective(def="*") {
+    return function(adj="a hard worker") {
+        return `You are ${def}${adj}${def}!`
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
